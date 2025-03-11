75e49c9f54ecd3cecdd0b79af38e0f12ee838cdb
marcolaxyz7 <marcoantoniogorzoni34@gmail.com>
2025-03-10 22:50:26 -0300

    AulaEXemplo1

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import Constants from 'expo-constants';
+
+//Exemplos
+import Exemplo01 from './src/exemplos/exemplo-01';
+
+//Ativigdades
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Texto Teste</Text>
-      <StatusBar style="auto" />
+      <Exemplo01 />
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -15,9 +20,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'orange',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: Constants.statusBarHeight,
+    backgroundColor: 'black',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+    padding: 8,
+    paddingBlock: Constants.statusBarHeight,
+    //marginTop: Constants.statusBarHeight,
   },
 });
---
+++ src/exemplos/exemplo-01/index.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+
+import styles from './style';
+
+function Exemplo01 () {
+    return(
+        <View style={styles.container}>
+            <Text>Exemplo 1</Text>
+            <Text>Aula PAM I, primeiro componente.</Text>
+        </View>
+    );
+}
+
+export default Exemplo01;
