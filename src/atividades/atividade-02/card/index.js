fbbed7b064ce71fcda5e1a636d7d1e9c037c2ce2
marcolaxyz7 <marcoantoniogorzoni34@gmail.com>
2025-03-31 22:49:48 -0300

    Atividade02
    comparando os erros da atividade e arrumando

--- src/atividades/atividade-02/card/index.js
+++ src/atividades/atividade-02/card/index.js
@@ -1,12 +1,16 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from 'react-native';
 
-import styles from "./styles";
+import styles from './styles';
 
-function Mensagem({titulo, children}) {
+function Mensagem({titulo, descricao, valor, img}) {
     return(
         <View style={styles.container}>
+           <Image source={img} style={styles.imagem} />
+           <View style={styles.containerTexto}>
             <Text style={styles.titulo}>{titulo}</Text>
-            <Text style={styles.children}>{children}</Text>
+            <Text style={styles.descricao}>{descricao}</Text>
+            <Text style={[styles.titulo, styles.valor]}>{R$`${valor}`}</Text>
+           </View>
         </View>
     );
 }
--- src/atividades/atividade-02/index.js
+++ src/atividades/atividade-02/index.js
@@ -1,4 +1,4 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text } from 'react-native';
 
 import styles from './styles';
 
@@ -11,26 +11,21 @@ import img3 from '../../../assets/polopreta.jpg';
 import img4 from '../../../assets/poloazul.jpg';
 
 
-import Mensagem from './mensagens';
+import Card from './mensagens';
 
 function Atividade02 () {
     return(
-        <><View style={styles.container}>
+        <View style={styles.container}>
             <Text style={styles.titulo}>Atividade 02</Text>
 
-            <Mensagem titulo={'Polo Branca'}>
-                <Image source={img1} style={styles.imagem} />
-                R$ 300
-            </Mensagem>
-            <Mensagem titulo={'Polo Vermelha'}>
-                <Image source={img2} style={styles.imagem} />
-                R$ 200
-            </Mensagem>
-            <Mensagem titulo={'Polo Preta'}>
-                <Image source={img3} style={styles.imagem} />
-                R$ 300
-            </Mensagem>
-            </View></>
+            <Card 
+            titulo={'Polo Branca'}
+            descricao={''}
+            valor={''}
+            img1={''}
+            />
+      
+        </View>
     );
 }
 
