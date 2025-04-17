950a6f1cca414bff13abade8784fe7efad83ee18
marcolaxyz7 <marcoantoniogorzoni34@gmail.com>
2025-04-16 22:46:29 -0300

    Exemplo05
    correção da atividade 04 e começando o exemplo05

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import Exemplo01 from './src/exemplos/exemplo-01';
 import Exemplo02 from './src/exemplos/exemplo-02';
 import Exemplo03 from './src/exemplos/exemplo-03';
 import Exemplo04 from './src/exemplos/exemplo-04';
+import Exemplo05 from './src/exemplos/exemplo-05';
 
 //Ativigdades
 import Atividade01 from './src/atividades/atividade-01';
@@ -19,7 +20,7 @@ import Atividade04 from './src/atividades/atividade-04';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Atividade04 />
+      <Exemplo05 />
       <StatusBar style="light" />
     </View>
   );
--- src/exemplos/exemplo-05/index.js
+++ src/exemplos/exemplo-05/index.js
@@ -3,51 +3,51 @@ import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
-function Atividade04 () {
-    const [nome, setNome] = useState('');
-    const [sobrenome, setSobrenome] = useState('');
+function Exemplo05 () {
+    const [n1, setN1] = useState('');
+    const [n2, setN2] = useState('');
     const [resultado, setResultado] = useState('');
     const [exibido, setExibido] = useState(false); // flag pra controlar se já exibiu
 
-    function exibirNomeCompleto() {
-        const nomeCompleto = `${nome} ${sobrenome}`;
+    function exibirResultado() {
+        const nomeCompleto = `${n1} ${n2}`;
         setResultado(nomeCompleto);
-        setNome('');
-        setSobrenome('');
+        setN1('');
+        setN2('');
         setExibido(true);
     }
 
     function reiniciar() {
         setExibido(false);
         setResultado('');
-        setNome('');
-        setSobrenome('');
+        setN1('');
+        setN2('');
     }
 
     return (
         <View style={styles.container}>
-            <Text style={styles.titulo}>Atividade 4</Text>
+            <Text style={styles.titulo}>Exemplo 5</Text>
 
             {/* Aqui vai aparecer o nome completo ou o texto padrão */}
             <Text style={styles.titulo3}>
-                {exibido ? resultado : 'Inserir nome e sobrenome'}
+                {exibido ? resultado : 'Inserir n1 e n2'}
             </Text>
 
             {/* Só mostra os campos se ainda não exibiu o nome */}
             {!exibido && (
                 <>
-                    <Text style={styles.titulo2}>Nome</Text>
+                    <Text style={styles.titulo2}>Valor 1</Text>
                     <TextInput 
                         style={styles.txtInput} 
                         value={nome}
-                        onChangeText={setNome}
+                        onChangeText={setN1}
                     />
 
-                    <Text style={styles.titulo2}>Sobrenome</Text>
+                    <Text style={styles.titulo2}>Valor 2</Text>
                     <TextInput 
                         style={styles.txtInput} 
                         value={sobrenome}
-                        onChangeText={setSobrenome}
+                        onChangeText={setN2}
                     />
                 </>
             )}
@@ -56,9 +56,9 @@ function Atividade04 () {
             {!exibido && (
                 <TouchableOpacity
                     style={styles.botao}
-                    onPress={exibirNomeCompleto}
+                    onPress={exibirResultado}
                 >
-                    <Text style={styles.txtBotao}>Exibir texto</Text>
+                    <Text style={styles.txtBotao}>Exibir Resultado</Text>
                 </TouchableOpacity>
             )}
 
@@ -75,4 +75,4 @@ function Atividade04 () {
     );
 }
 
-export default Atividade04;
+export default Exemplo05;
